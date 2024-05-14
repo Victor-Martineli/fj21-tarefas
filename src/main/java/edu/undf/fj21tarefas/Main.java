f68df455c858476d6e482995a7c26e3d9fab3d5d
Aluno <victor.martineli999@gmail.com>
2024-05-13 22:42:14 -0300

    a

---
+++ src/main/java/edu/undf/fj21tarefas/Main.java
@@ -0,0 +1,15 @@
+package edu.undf.fj21tarefas;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class Main {
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(Main.class, args);
+
+    }
+}
